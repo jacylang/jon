3e1593ebd9fca667a2e774def03a98243b2a031c
is_gordeev <gordeev.john@gmail.com>
2021-07-31 20:45:39 +0300

    Update `Lexer`, parse `true`

--- include/Lexer.h
+++ include/Lexer.h
@@ -261,6 +261,12 @@ namespace jon {
                 return;
             }
 
+            if (isSeq('t', 'r', 'u', 'e')) {
+                advance(4);
+                addToken(TokenKind::True);
+                return;
+            }
+
             // Identifier is anything not containing specific tokens
             std::string val;
             while (not eof()) {
