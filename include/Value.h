3985342bc4732315296c0e0c4866d16720b78eb3
is_gordeev <gordeev.john@gmail.com>
2021-08-01 12:30:56 +0300

    Update `Value`s

--- include/Value.h
+++ include/Value.h
@@ -3,10 +3,15 @@
 
 #include <variant>
 #include <memory>
+#include <string>
+#include <assert.h>
+
+#include "Lexer.h"
 
 namespace jon::val {
     struct Value;
     using value_ptr = std::unique_ptr<Value>;
+    using int_t = int64_t;
 
     enum class Type {
         Null,
@@ -25,6 +30,43 @@ namespace jon::val {
     };
 
     const auto null = Value(Type::Null);
+
+    struct Bool : Value {
+        Bool(const Token & token) : Value(Type::Bool) {
+            assert(token.kind == TokenKind::True or token.kind == TokenKind::False);
+            val = token.kind == TokenKind::True;
+        }
+
+        bool val;
+    };
+
+    struct Int : Value {
+        Int(const Token & token) : Value(Type::Int) {
+            assert(token.kind == TokenKind::DecInt or token.kind == TokenKind::HexInt or
+                   token.kind == TokenKind::OctoInt or token.kind == TokenKind::BinInt);
+            val = std::stoull(token.val, nullptr);
+        }
+
+        int_t val;
+    };
+
+    struct Float : Value {
+        Float(double val) : Value(Type::Float), val(val) {}
+
+        double val;
+    };
+
+    struct String : Value {
+        String(const std::string & val) : Value(Type::String), val(val) {}
+
+        std::string val;
+    };
+
+    struct Object : Value {
+        String() : Value(Type::String), val(val) {}
+
+        std::string val;
+    };
 }
 
 #endif // JON_VALUE_H
