18d46cde762be2ec33736c1f1511958d81c3e006
hazer-hazer <gordeev.john@gmail.com>
2021-08-02 19:29:44 +0300

    Add `Schema::typeNames`

--- include/jon/schema.h
+++ include/jon/schema.h
@@ -10,9 +10,22 @@ namespace jon {
         ~Schema() = default;
 
         void validate(const jon & value, const jon & schema) {
-            std::string type = schema.at("type").check(jon::Type::String).get<jon::str_t>();
-            
+            std::string typeName = schema.at("type").check(jon::Type::String).get<jon::str_t>();
+
         }
+
+    private:
+        static const std::map<std::string, jon::Type> typeNames;
+    };
+
+    const std::map<std::string, jon::Type> Schema::typeNames = {
+        {"null", jon::Type::Null},
+        {"bool", jon::Type::Bool},
+        {"int", jon::Type::Int},
+        {"float", jon::Type::Float},
+        {"string", jon::Type::String},
+        {"object", jon::Type::Object},
+        {"array", jon::Type::Array},
     };
 }
 
