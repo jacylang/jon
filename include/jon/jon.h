6648ee6fceb61238b8c7485178da7b648351a632
is_gordeev <gordeev.john@gmail.com>
2021-08-03 00:41:34 +0300

    Update `jon`

--- include/jon/jon.h
+++ include/jon/jon.h
@@ -316,12 +316,6 @@ namespace jon {
         }
 
         template<class T>
-        const T & at(const str_t & key) const {
-            value.assertTypeObject(key);
-            return value.get<obj_t>().at(key).get<T>();
-        }
-
-        template<class T>
         T & at(const str_t & key) {
             value.assertObjectFirstAccess(key);
             return value.get<obj_t>().at(key).get<T>();
