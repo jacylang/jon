ae3b4473e632f35733370812fe96988ba230c71c
hazer-hazer <gordeev.john@gmail.com>
2021-08-02 19:04:06 +0300

    Add `jon::typeStr` method

--- include/jon/jon.h
+++ include/jon/jon.h
@@ -34,6 +34,18 @@ namespace jon {
             Array,
         };
 
+        static std::string typeStr(Type type) {
+            switch (type) {
+                case Type::Null: return "null";
+                case Type::Bool: return "boolean";
+                case Type::Int: return "integer";
+                case Type::Float: return "float";
+                case Type::String: return "string";
+                case Type::Object: return "object";
+                case Type::Array: return "array";
+            }
+        }
+
     private:
         struct Value {
             explicit Value() = default;
@@ -128,15 +140,7 @@ namespace jon {
             }
 
             std::string typeStr() const {
-                switch (t) {
-                    case Type::Null: return "null";
-                    case Type::Bool: return "boolean";
-                    case Type::Int: return "integer";
-                    case Type::Float: return "float";
-                    case Type::String: return "string";
-                    case Type::Object: return "object";
-                    case Type::Array: return "array";
-                }
+                return jon::typeStr(t);
             }
 
             Type t;
