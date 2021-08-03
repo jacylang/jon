4463d5cfbb908194387a99971ea23b2f3d2f6278
hazer-hazer <gordeev.john@gmail.com>
2021-08-03 17:25:43 +0300

    Update `error` constructors

--- include/jon/error.h
+++ include/jon/error.h
@@ -12,19 +12,19 @@ namespace jon {
     };
 
     struct parse_error : jon_exception {
-        parse_error(const std::string & msg) : jon_exception(msg) {}
+        parse_error(const std::string & msg) : jon_exception("[jon::parse_error]: " + msg) {}
     };
 
     struct out_of_range : jon_exception {
-        out_of_range(const std::string & msg) : jon_exception(msg) {}
+        out_of_range(const std::string & msg) : jon_exception("[jon::out_of_range] " + msg) {}
     };
 
     struct validation_error : jon_exception {
-        validation_error(const std::string & msg) : jon_exception(msg) {}
+        validation_error(const std::string & msg) : jon_exception("[jon::validation_error]" + msg) {}
     };
 
     struct invalid_schema : jon_exception {
-        invalid_schema(const std::string & msg) : jon_exception(msg) {}
+        invalid_schema(const std::string & msg) : jon_exception("[jon::invalid_schema]: " + msg) {}
     };
 }
 
