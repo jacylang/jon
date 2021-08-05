17334da099b1e6dbf72b7319dd03782814d47406
is_gordeev <gordeev.john@gmail.com>
2021-08-06 00:01:16 +0300

    Update `unit-ctor` test

--- test/src/unit-ctor.cpp
+++ test/src/unit-ctor.cpp
@@ -22,18 +22,22 @@ TEST_CASE("constructors") {
         SUBCASE ("bool_t") {
             auto bool_t_ctor = jon {jon::bool_t {}};
             CHECK(bool_t_ctor.type() == jon::Type::Bool);
+            CHECK(bool_t_ctor == false);
         }
         SUBCASE ("int_t") {
             auto int_t_ctor = jon {jon::int_t {}};
             CHECK(int_t_ctor.type() == jon::Type::Int);
+            CHECK(int_t_ctor == 0);
         }
         SUBCASE ("float_t") {
             auto float_t_ctor = jon {jon::float_t {}};
             CHECK(float_t_ctor.type() == jon::Type::Float);
+            CHECK(float_t_ctor == 0.0);
         }
         SUBCASE ("str_t") {
             auto str_t_ctor = jon {jon::str_t {}};
             CHECK(str_t_ctor.type() == jon::Type::String);
+            CHECK(str_t_ctor == "");
         }
         SUBCASE ("obj_t") {
             auto obj_t_ctor = jon {jon::obj_t {}};
